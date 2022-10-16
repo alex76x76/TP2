be0aa12d89958a1de51c5f2b43666c4da82a65ec
alex76x76 <114753082+alex76x76@users.noreply.github.com>
2022-10-16 17:08:08 +0300

    Update unit.py

--- unit.py
+++ unit.py
@@ -13,21 +13,21 @@ class utest(unittest.TestCase):
     
     def test_min(self):
         self.assertTrue(_min(array) == -9)
-        self.assertTrue(_min(arraymin) == -9991)
+        self.assertTrue(_min(arraymin) == -7)
         self.assertTrue(_min(arraymax) == -3)
         self.assertTrue(_min(arraymult) == -3000)
         self.assertTrue(_min(arraytim) == -3000)
 
     def test_max(self):
         self.assertTrue(_max(array) == 9)
-        self.assertTrue(_max(arraymin) == 9981)
+        self.assertTrue(_max(arraymin) == 7)
         self.assertTrue(_max(arraymax) == 3)
         self.assertTrue(_max(arraymult) == 3000)
         self.assertTrue(_max(arraytim) == 3000)
     
     def test_sum(self):
         self.assertTrue(_sum(array) == 20)
-        self.assertTrue(_sum(arraymin) == -45404)
+        self.assertTrue(_sum(arraymin) == -0)
         self.assertTrue(_sum(arraymax) == 0)
         self.assertTrue(_sum(arraymult) == 16267)
         self.assertTrue(_sum(arraytim) == -297327)
